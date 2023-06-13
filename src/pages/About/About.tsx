14dd97a306dab3b46b3957b834f5542aab2ce19d
LuanGiomo <luanmgiomo@gmail.com>
2023-06-13 20:55:24 -0300

    Ajuste tamanho de font about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
         <h2 className="mb-3 text-left text-zinc-300 text-xl font-semibold tracking-tight sm:text-2xl">
           Quem Somos
         </h2>
-        <p className="text-justify leading-7 text-zinc-400 md:text-left">
+        <p className="text-justify leading-7 text-sm text-zinc-400 md:text-left sm:text-base sm:leading-7">
           Transformar diversão em competição e depois em resultados positivos,
           sem perder a alegria e o foco. Foi com esse desafio (e inspiração!)
           que a FURIA surgiu em 2017. Se no início a empreitada parecia ousada,
